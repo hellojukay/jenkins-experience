6b618842c6fbd892814f8950d379b3cce3dbbe97
李聪 <licong@360.net>
2019-05-30 14:23:45 +0800

    add git list remote tags

--- src/git_list_tag.groovy
+++ src/git_list_tag.groovy
@@ -1,33 +1,3 @@
-# jenkins-experience
-
-some experience about jenkins
-
-## problems
-
-### git clone to special dir
-```groovy
-checkout scm: [$class: 'GitSCM',extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'deploy-smac']], userRemoteConfigs: [[url: "${REPO}", credentialsId: "${credential}"]], branches: [[name: "${VERSION}"]]],poll: false
-```
-
-### read ini config
-```groovy
-def call(String file) {
-    def m = [:]
-    def lines = new File(file).readLines()
-    for(line in lines) {
-        def kv = line.split("=")
-        if(kv.length == 2) {
-            def k = kv[0]
-            def v = kv[1]
-            m[k] = v
-        }
-    }
-    return m
-}
-```
-
-### list git remote tags 
-```groovy
 def call(String git_url, String credentialsId = "gitlab") {
     def GIT_USER = ""
     def GIT_PW = ""
@@ -43,4 +13,3 @@ def call(String git_url, String credentialsId = "gitlab") {
         it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '').replaceAll("\\^\\{\\}", '')
     }
 }
-```
